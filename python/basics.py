f2775448dca7fd831e609d31dae77246453d5a95
User 01 <user01@unst.(none)>
2017-03-01 15:36:49 +0000

    changed basics.py

--- python/basics.py
+++ python/basics.py
@@ -8,3 +8,43 @@ a = ((b**2 + c**2)**0.5)
 print type (a)
 print type (b)
 print type (c)
+
+str(a) + "..." + str(b) + "..." + str(c) "..."
+
+while 1:
+    x = 1
+while 0:
+    x = 0
+
+mylist = [23, "hi", 2.4e-10]
+count = 0
+while count < 3:
+    mylist[count] = item
+    print item, mylist.index(item)
+    num_moonscount += 1
+
+x = 1
+if x: print x, "is true"
+
+# lists and slicing
+
+print mylist[1]
+print mylist[-2]
+print mylist [:]
+print mylist [1:4]
+
+#tuples
+t = (1,)
+print t [-1]
+m = range (100, 201)
+tup = tuple (m)
+print tup[0], tup[-1]
+
+mylist = [23, "hi", 2.4e-10]
+for (count,item) in enumerate (mylist):
+    print count, item
+
+(first, middle, last) = mylist
+print first, middle, last
+(first, middle, last) = (last, middle, first)
+print last, middle, firstc
